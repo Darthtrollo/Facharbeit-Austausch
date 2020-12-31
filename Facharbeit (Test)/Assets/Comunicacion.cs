e4fcb0b01c62e372ece1ef5fcc387b28a63889bd
Darthtrollo <76814737+Darthtrollo@users.noreply.github.com>
2020-12-31 15:38:09 +0100

    First commited
    dies das

---
+++ Facharbeit (Test)/Assets/Comunicacion.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class Comunicacion : MonoBehaviour
+{
+
+    #region Instance
+    private static Comunicacion instance;
+    public static Comunicacion Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<Comunicacion>();
+                if (instance == null)
+                {
+                    instance = new GameObject("Spawned GyroManager", typeof(Comunicacion)).GetComponent<Comunicacion>();
+                }
+            }
+
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+
+    }
+    #endregion
+
+    SerialPort stream = new SerialPort("COM3", 9600);
+    public string receivedstring;
+    public GameObject carrito;
+    public Vector3 rot;
+    public Vector3 rot2;
+    public string[] datos;
+    public string[] datos_recibidos;
+
+
+    void Start()
+    {
+        stream.Open(); //Open the Serial Stream.
+    }
+
+    void Update()
+    {
+        receivedstring = stream.ReadLine(); //Read the information
+        stream.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+
+        string[] datos = receivedstring.Split('|'); //My arduino script returns a 3 part value (IE: 12,30,18)
+        if (datos[0] != "" && datos[1] != "" && datos[2] != "") //Check if all values are recieved
+        {
+            datos_recibidos[0] = datos[0];
+            datos_recibidos[1] = datos[1];
+            datos_recibidos[2] = datos[2];
+
+
+            //Read the information and put it in a vector3
+
+            //Take the vector3 and apply it to the object this script is applied.
+            stream.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+        }
+    }
+}
---
+++ Facharbeit (Test)/Assets/MeshGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+
+public class MeshGenerator : MonoBehaviour
+{
+    public float xPosition;
+    public float yPosition;
+    public float zPosition;
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    public Vector3 Position;
+
+
+    void Start()
+    {
+        xPosition = float.Parse(Comunicacion.Instance.datos_recibidos[0]); // hier gibt es das Problem
+        yPosition = float.Parse(Comunicacion.Instance.datos_recibidos[1]);
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateShape();
+        UpdateMesh();
+       // Debug.Log(mesh.vertices.Length);
+        mesh.vertices = vertices;
+        Debug.Log(xPosition);
+
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+           new Vector3 ()
+        };
+
+        triangles = new int[]
+        {
+            
+
+        };
+    }
+
+    private void OnDrawGizmos()
+    {
+       // wenn fehler mit String zu float convert behoben ist, kann man hier dann die Punkte anzeigen lassen.
+    }
+
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+    }   
+}
